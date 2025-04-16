a8efe2f279c4c9d268afbe93adcd66cae9529ab3
unknown <qw5nzwx@gmail.com>
2025-04-16 13:43:15 -0500

    ...

--- SistemasEcuaciones/SisEcu.py
+++ SistemasEcuaciones/SisEcu.py
@@ -1,18 +1,20 @@
 import numpy as np
 
+
 def resolver_sistema():
     print(" Sistema de Ecuaciones Lineales con NumPy")
-    
+
     n = int(input("Ingrese el número de incógnitas: "))
-    
+
     A = []
     b = []
 
     print("Ingrese los coeficientes de cada ecuación:")
     for i in range(n):
-        fila = list(map(float, input(f"Coeficientes de la ecuación {i+1} separados por espacio: ").split()))
+        fila = list(map(float, input(
+            f"Coeficientes de la ecuación {i+1} separados por espacio: ").split()))
         A.append(fila)
-    
+
     print("Ingrese los términos independientes:")
     b = list(map(float, input("Separados por espacio: ").split()))
 
@@ -28,5 +30,5 @@ def resolver_sistema():
         print("\n El sistema no tiene solución única:")
         print(str(e))
 
-# Ejecutar el programa
+
 resolver_sistema()
